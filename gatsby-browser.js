640b56ca63a1317fd03fa504418cc9159d4df133
Glorieux Lukama <ir.antoineglorieux@gmail.com>
2023-06-15 13:24:26 +0200

    📦 NEW: barlow and dm sans fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,5 @@
  */
 
 // You can delete this file if you're not using it
+import "@fontsource/barlow";
+import "@fontsource/dm-sans";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
+import { Box, Heading, Text } from "@chakra-ui/react"
 import { PageProps } from "gatsby"
 import React from "react"
 
 type Props = {}
 
 const IndexPage: React.FC<PageProps> = (props: Props) => {
-  return <div>IndexPage</div>
+  return (
+    <Box>
+      <Heading textTransform="uppercase">This is the header</Heading>
+      <Text textTransform="uppercase">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem earum
+        mollitia a assumenda ut qui nulla nemo sunt vitae hic odit aspernatur
+        impedit sapiente voluptas, officiis doloremque placeat aliquam deserunt?
+      </Text>
+    </Box>
+  )
 }
 
 export default IndexPage
